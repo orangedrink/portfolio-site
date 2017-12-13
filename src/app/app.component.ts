e6cfe15e87c3996ae956c8433c253543a2ed0315
unknown <david.franks1@gmail.com>
2017-12-12 20:08:01 -0600

    Adds moch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { BandosComponent } from './bandos/bandos.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Bando Hacker';
 }
--- src/app/bandos/bandos.component.ts
+++ src/app/bandos/bandos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BandoComponent } from './bando/bando.component'
 @Component({
   selector: 'app-bandos',
   templateUrl: './bandos.component.html',
