417499626e1449e71e5cf5055f8b1ca830b90c93
unknown <david.franks1@gmail.com>
2018-03-12 19:08:40 -0500

    Improves command logic

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -97,27 +97,31 @@ export class PagesComponent implements OnInit {
 
 
   command(queryString: string = '', terminalWindow = null,  notesWindow = null) {
-    if(terminalWindow && notesWindow){
-      this.toTop('terminal');
-    }
+    this.toTop('terminal');
     this.inputCommand = '';
     //this.pages = [];
     this.loading = true;
     this.cmdLines.push(`${this.prompt} ${queryString}`);
     this.historyIndex = this.cmdLines.length;
     
-    if(queryString=='pages'){
-      this.cmdLines.push(`pages command syntax:`);
-      this.pages.push("test");
+    if(queryString=='about'||queryString=='portfolio'||queryString=='games'){
+      this.cmdLines.push(`${queryString} command syntax:`);
+      this.pages.push(queryString);
       this.toTop('notes')
-      this.loading = false;
-      setTimeout(this.scrollTerminal, 10);
+    }else if(queryString=='secret'){
+      this.cmdLines.push(`DIS THE SEEKRET COMMAND`);
+      this.pages.push("SEEKRET");
+      this.pages.push("COMMAD");
+      this.toTop('notes')
+    }else if(queryString=='help'){
+      this.cmdLines.push(`help command syntax:`);
     }else{
       this.cmdLines.push(`Command not found: ${queryString}`);
-      this.loading = false;
-      setTimeout(this.scrollTerminal, 10);
     }
- /* this.chicagoOpenDataService.getBandos(queryString.replace(/ /g, '&')).subscribe(data => {
+    this.loading = false;
+    setTimeout(this.scrollTerminal, 10);
+
+    /* this.chicagoOpenDataService.getBandos(queryString.replace(/ /g, '&')).subscribe(data => {
       this.bandos = data;
       this.cmdLines.push(`Success: ${data.length} bandos found.`);
       this.loading = false;
